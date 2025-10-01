e42e59a5bc705e71a8feeab1dbf60952b3d2d367
Jesse <rchikerema@gmail.com>
2025-10-01 17:56:40 +1300

    vercel config done

--- ruruB/views.py
+++ ruruB/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
+# Create your views here.
+
+def index(request):
+    return render(request, template_name="index.html")
+
+
+# Create your views here.
+from django.shortcuts import render
+
 # Create your views here.
